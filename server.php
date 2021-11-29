e438524e8bbab8058c3cca4a2e5c59e07204bb84
avatar-lavventura <alper.alimoglu@gmail.com>
2021-11-29 09:44:14 +0000

    Fix README.md

--- server.php
+++ server.php
@@ -1,14 +1,11 @@
 <?php
 if(!empty($_POST) && !empty($_POST["ClientOutput"])) {
-  //get the object from client
-  $serverVar = json_decode($_POST["ClientOutput"]);
-
-  //do something with the object
-  if (!empty(account) && $serverVar->flag == 1 ) {
-        $doo = shell_exec("echo ". $serverVar->account ." " . $serverVar->account ." >  /eBloc/fifo");
-	$doo = shell_exec("echo ". $serverVar->account ." " . $serverVar->orcid ." >  /eBloc/orcid.txt");
-  }
-
-  //send it back to client
-  echo json_encode($serverVar);
+    //get the object from client
+    $server_var = json_decode($_POST["ClientOutput"]);
+    if (!empty(account) && $server_var->flag == 1 ) {
+        $output = shell_exec("echo ". $server_var->account ." " . $server_var->account ." > /eBloc/fifo");
+        $output = shell_exec("echo ". $server_var->account ." " . $server_var->orcid ." > /eBloc/orcid.txt");
+    }
+    //send it back to client
+    echo json_encode($server_var);
 }
